3136bf983e08419d173ef882a406815f607ea648
Dennis Lamken <lamken@otaris.de>
2021-06-03 08:47:16 +0200

    license update

--- src/main/java/de/metahlfabric/MetaDef.java
+++ src/main/java/de/metahlfabric/MetaDef.java
@@ -9,9 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The MetaDef defines the attributes and products available in this channel
+ * The MetaDef defines the attributes and assets available in this channel.
+ *
+ * @author Tobias Wagner, Dennis Lamken
+ *
+ * Copyright 2021 OTARIS Interactive Services GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 @DataType()
 public class MetaDef {
 
